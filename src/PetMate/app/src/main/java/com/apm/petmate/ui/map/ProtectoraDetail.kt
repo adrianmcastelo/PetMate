d897a367c119af250f467c3eb6a24a487ab6432c
Adrián Delfín Mella Castelo <adrian.mcastelo@udc.es>
2023-06-13 01:28:20 +0200

    Linked protectora details and aesthetic fixes

--- src/PetMate/app/src/main/java/com/apm/petmate/ui/map/ProtectoraDetail.kt
+++ src/PetMate/app/src/main/java/com/apm/petmate/ui/map/ProtectoraDetail.kt
@@ -1,93 +1,37 @@
 package com.apm.petmate.ui.map
 
-import android.R
-import android.graphics.Bitmap
-import android.graphics.BitmapFactory
 import android.os.Bundle
-import android.text.method.LinkMovementMethod
-import android.util.Base64
 import androidx.appcompat.app.AppCompatActivity
-import com.android.volley.Request
-import com.android.volley.toolbox.JsonObjectRequest
-import com.apm.petmate.databinding.ActivityProtectoraDetailBinding
-import com.apm.petmate.utils.Protectora
-import com.apm.petmate.utils.VolleyApi
-import org.json.JSONObject
+import androidx.fragment.app.Fragment
+import com.apm.petmate.R
+import com.apm.petmate.ui.protectoraDetail.ProtectoraDetailFragment
 
 
 class ProtectoraDetail : AppCompatActivity() {
 
-    private lateinit var binding: ActivityProtectoraDetailBinding
+    var id: Int? = null
+        get() = field
 
-    private var token: String? = null
-    private var idProtectora: Int? = null
-    private lateinit var protectora: Protectora
+    var token:String? = null
+        get() = field
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityProtectoraDetailBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_protectora_detail)
 
-        idProtectora = this.intent.extras?.getInt("idProtectora")
-        token = this.intent.extras?.getString("token")
+        this.id = this.intent.extras?.getInt("idProtectora")
+        this.token = this.intent.extras?.getString("token")
 
-        println("ID :" + idProtectora)
+        println("ID :" + id)
         println("TOKEN :" + token)
 
-        getProtectora()
+        val fragment: Fragment = ProtectoraDetailFragment()
+        moveToFragment(fragment)
+
     }
 
-    private fun setProtectora(protectora: Protectora) {
-        binding.protectoraImage.setImageBitmap(protectora.imagen)
-        binding.protectoraName.text = protectora.name
-        binding.protectoraDescription.text = protectora.descripcion
-        binding.protectoraPhone.text = protectora.telefono
-        binding.protectoraMail.text = protectora.correo
-        val protectoraUrl = binding.protectoraWeb
-        protectoraUrl.movementMethod = LinkMovementMethod.getInstance()
-        binding.protectoraLocation.text = protectora.direccion + ", " + protectora.ubicacion
-        binding.protectoraWeb.text = protectora.url
-    }
-
-    private fun getProtectora() {
-        val url = "http://10.0.2.2:8000/petmate/protectora/" + idProtectora
-        val request = object: JsonObjectRequest(
-            Request.Method.GET, url, null,
-            { response ->
-                protectora = parseProtectora(response)
-                setProtectora(protectora)
-            },
-            { error -> error.printStackTrace() }
-        ){
-            override fun getHeaders(): MutableMap<String, String> {
-                val headers = HashMap<String, String>()
-                headers["Authorization"] = "Token " + token
-                return headers
-            }
-        }
-        VolleyApi.getInstance(this).addToRequestQueue(request)
-    }
-
-    private fun parseProtectora(response: JSONObject): Protectora {
-        var protectora: Protectora = Protectora()
-
-        protectora.id = response.getInt("id")
-        protectora.name = response.getString("name").toString()
-        protectora.direccion = response.getString("direccion").toString()
-        protectora.ubicacion = response.getString("ubicacion").toString()
-        protectora.telefono = response.getString("telefono").toString()
-        protectora.url = response.getString("url").toString()
-        protectora.correo = response.getString("correo").toString()
-        protectora.descripcion = response.getString("descripcion").toString()
-        protectora.latitud = response.getDouble("latitud")
-        protectora.longitud = response.getDouble("longitud")
-        protectora.imagen = base64ToBitmap(response.getString("imagen").toString())
-
-        return protectora
-    }
-
-    private fun base64ToBitmap(image: String): Bitmap? {
-        val imageBytes = Base64.decode(image, 0)
-        val imageBitmap = BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.size)
-        return imageBitmap
+    private fun moveToFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_frame, fragment, fragment.javaClass.simpleName).commit()
     }
 }
--- src/PetMate/app/src/main/java/com/apm/petmate/ui/protectoraDetail/ProtectoraDetailFragment.kt
+++ src/PetMate/app/src/main/java/com/apm/petmate/ui/protectoraDetail/ProtectoraDetailFragment.kt
@@ -1,22 +1,109 @@
 package com.apm.petmate.ui.protectoraDetail
 
+import android.app.Activity
 import android.os.Bundle
+import android.text.method.LinkMovementMethod
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.android.volley.Request
+import com.android.volley.toolbox.JsonObjectRequest
+import com.apm.petmate.MainActivity
 import com.apm.petmate.R
+import com.apm.petmate.databinding.FragmentAnimalsBinding
+import com.apm.petmate.databinding.FragmentProtectoraDetailBinding
+import com.apm.petmate.ui.map.ProtectoraDetail
+import com.apm.petmate.utils.AnimalUtils
+import com.apm.petmate.utils.Protectora
+import com.apm.petmate.utils.VolleyApi
+import org.json.JSONObject
 
 
 class ProtectoraDetailFragment : Fragment() {
 
+    private lateinit var binding: FragmentProtectoraDetailBinding
+
+    private var token: String? = null
+    private var idProtectora: Int? = null
+    private lateinit var protectora: Protectora
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_protectora_detail, container, false)
+        super.onCreate(savedInstanceState)
+        binding = FragmentProtectoraDetailBinding.inflate(layoutInflater)
+
+        val root: View = binding.root
+
+        if ((activity as? MainActivity)?.id == null) {
+            this.idProtectora = (activity as? ProtectoraDetail)?.id
+        } else {
+            this.idProtectora = (activity as? MainActivity)?.id
+        }
+
+        if ((activity as? MainActivity)?.token == null) {
+            this.token = (activity as? ProtectoraDetail)?.token
+        } else {
+            this.token = (activity as? MainActivity)?.token
+        }
+
+        println("ID :" + idProtectora)
+        println("TOKEN :" + token)
+
+        getProtectora()
+
+        return root
+    }
+
+    private fun setProtectora(protectora: Protectora) {
+        binding.protectoraImage.setImageBitmap(protectora.imagen)
+        binding.protectoraName.text = protectora.name
+        binding.protectoraDescription.text = protectora.descripcion
+        binding.protectoraPhone.text = protectora.telefono
+        binding.protectoraMail.text = protectora.correo
+        val protectoraUrl = binding.protectoraWeb
+        protectoraUrl.movementMethod = LinkMovementMethod.getInstance()
+        binding.protectoraLocation.text = protectora.direccion + ", " + protectora.ubicacion
+        binding.protectoraWeb.text = protectora.url
+    }
+
+    private fun getProtectora() {
+        val url = "http://10.0.2.2:8000/petmate/protectora/" + idProtectora
+        val request = object: JsonObjectRequest(
+            Request.Method.GET, url, null,
+            { response ->
+                protectora = parseProtectora(response)
+                setProtectora(protectora)
+            },
+            { error -> error.printStackTrace() }
+        ){
+            override fun getHeaders(): MutableMap<String, String> {
+                val headers = HashMap<String, String>()
+                headers["Authorization"] = "Token " + token
+                return headers
+            }
+        }
+        VolleyApi.getInstance(getActivity() as Activity).addToRequestQueue(request)
+    }
+
+    private fun parseProtectora(response: JSONObject): Protectora {
+        var protectora: Protectora = Protectora()
+
+        protectora.id = response.getInt("id")
+        protectora.name = response.getString("name").toString()
+        protectora.direccion = response.getString("direccion").toString()
+        protectora.ubicacion = response.getString("ubicacion").toString()
+        protectora.telefono = response.getString("telefono").toString()
+        protectora.url = response.getString("url").toString()
+        protectora.correo = response.getString("correo").toString()
+        protectora.descripcion = response.getString("descripcion").toString()
+        protectora.latitud = response.getDouble("latitud")
+        protectora.longitud = response.getDouble("longitud")
+        protectora.imagen = AnimalUtils().base64ToBitmap(response.getString("imagen").toString())
+
+        return protectora
     }
 
 
